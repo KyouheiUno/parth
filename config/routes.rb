155919a618d396036388275d56066a7282fef5ea
kyohei uno <waizeru00@gmail.com>
2021-02-04 23:20:30 +0900

    deviseを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
   
+  
+  # ルートパス
+  root to: "main#index"
   # user
   get 'users/index'
   get 'users/show'
-  # ルート
-  root to: "main#index"
+  # deviseのルーティング
+  devise_for :users
 
+  # ルーティーン記事投稿
+  resources :post_routines
+  get 'post_routines/confirm', to: 'post_routines#post_routines'
+
+  # routeの基本構文
+  #(get or post or patch or delete) 'URL', to: 'コントxローラー名#メソッド名'
 end
