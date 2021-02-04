5fd55fdf233a462d115259bb7c278c1622647ccc
kyohei uno <waizeru00@gmail.com>
2021-02-05 02:27:47 +0900

    [モデル・コントローラー作成] usersモデルとusersコントローラーを作成

---
+++ db/migrate/20210204170219_create_users.rb
@@ -0,0 +1,16 @@
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.string      :name
+      t.string       :email
+      t.text        :image
+      t.text        :profession
+      t.date        :birthday
+      t.string      :password_digest
+      t.timestamps
+    end
+
+    add_index :users, :email, :unique => true
+    
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_04_160511) do
+ActiveRecord::Schema.define(version: 2021_02_04_170219) do
 
   create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
+    t.string "name"
+    t.string "email"
+    t.text "image"
+    t.text "profession"
+    t.date "birthday"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "user_name"
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["user_name"], name: "index_users_on_user_name"
   end
 
 end
