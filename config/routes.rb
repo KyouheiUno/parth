ac2fc300c47a4f5af5117f1049820d7284abfaac
kyohei uno <waizeru00@gmail.com>
2021-02-05 01:52:24 +0900

    [mysqlとdeviseやめた] やめたファイルの一式を削除する

--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,11 @@ Rails.application.routes.draw do
   # user
   get 'users/index'
   get 'users/show'
-  # deviseのルーティング
-  devise_for :users
 
   # ルーティーン記事投稿
   resources :post_routines
   get 'post_routines/confirm', to: 'post_routines#post_routines'
 
   # routeの基本構文
-  #(get or post or patch or delete) 'URL', to: 'コントxローラー名#メソッド名'
+  #(get or post or patch or delete) 'URL', to: 'コントローラー名#メソッド名'
 end
